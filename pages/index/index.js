0a8c1dba05da391bd824e3888be2a678c015c8cd
wangdg <dlwangdg@gmail.com>
2018-06-27 12:09:23 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,25 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    items: [
+      {
+        image: '',
+        data: null,
+      },
+      {
+        image: '',
+        data: null,
+      },
+      {
+        image: '',
+        data: null,
+      }
+    ]
+  },
+  onLoad: function () {
+    
+  },
+})
